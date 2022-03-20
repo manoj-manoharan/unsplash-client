36e15d3b00c53a6b2a965e615495297caaec9d77
manoj-manoharan <yogeshwaranmanoharan@gmail.com>
2022-03-20 20:29:50 +0530

    - Improving image quality

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,7 +4,7 @@ import getWindowDimensions from '../lib/window-dimensions';
 const ImageCard = ({ image }) => {
     return (
         <>
-            <img src={image.urls.small} />
+            <img src={image.urls.regular} />
         </>
     );
 }
